b8b02cfaebad325707f536bf0a18f65c820da7ca
rodrigosy03 <74663928@certus.edu.pe>
2023-07-14 16:24:33 -0500

    eureka

--- ms-estudiantes/src/main/java/com/servicio/msestudiantes/MsEstudiantesApplication.java
+++ ms-estudiantes/src/main/java/com/servicio/msestudiantes/MsEstudiantesApplication.java
@@ -3,9 +3,11 @@ package com.servicio.msestudiantes;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 // import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 //@EnableFeignClients
 @SpringBootApplication
+@EnableEurekaClient
 public class MsEstudiantesApplication {
 
 	public static void main(String[] args) {
--- ms-inscripciones/src/main/java/com/servicio/msinscripciones/MsInscripcionesApplication.java
+++ ms-inscripciones/src/main/java/com/servicio/msinscripciones/MsInscripcionesApplication.java
@@ -2,8 +2,10 @@ package com.servicio.msinscripciones;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class MsInscripcionesApplication {
 
 	public static void main(String[] args) {
