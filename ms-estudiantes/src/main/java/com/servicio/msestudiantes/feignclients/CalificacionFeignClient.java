99f3879d121a820ec189fce90039e15b3c2127dc
rodrigosy03 <74663928@certus.edu.pe>
2023-07-14 17:54:46 -0500

    eureka 10

--- ms-estudiantes/src/main/java/com/servicio/msestudiantes/feignclients/CalificacionFeignClient.java
+++ ms-estudiantes/src/main/java/com/servicio/msestudiantes/feignclients/CalificacionFeignClient.java
@@ -1,15 +1,15 @@
 package com.servicio.msestudiantes.feignclients;
 
-import org.springframework.cloud.openfeign.FeignClient;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
+// import org.springframework.cloud.openfeign.FeignClient;
+// import org.springframework.web.bind.annotation.PostMapping;
+// import org.springframework.web.bind.annotation.RequestBody;
+// import org.springframework.web.bind.annotation.RequestMapping;
 
-import com.servicio.msestudiantes.modelos.Calificacion;
+// import com.servicio.msestudiantes.modelos.Calificacion;
 
-@FeignClient(name = "ms-calificaciones", url = "http://localhost:5003")
-@RequestMapping("/calificaciones")
+// @FeignClient(name = "ms-calificaciones", url = "http://localhost:5003")
+// @RequestMapping("/calificaciones")
 public interface CalificacionFeignClient {
-	@PostMapping("/crear")
-    public Calificacion crear(@RequestBody Calificacion calificacion);
+	// @PostMapping("/crear")
+    // public Calificacion crear(@RequestBody Calificacion calificacion);
 }
